d05e59385162c48a862bad36a99e33ab50cfd8c7
Emmanuel Antoniogo mez Martinez <shinji.ogami@gmail.com>
2020-06-03 15:17:27 -0500

    Update main.go

--- Ejer10/main.go
+++ Ejer10/main.go
@@ -8,13 +8,13 @@ type humano interface {
 	respirar()
 	pensar()
 	comer()
-	sexo()
+	sexo() string
 }
 
 type animal interface {
 	respirar()
 	comer()
-	carniboro() bool
+	Escarnivoro() bool
 }
 
 type vegetal interface {
@@ -30,39 +30,74 @@ type hombre struct {
 	respirando	bool
 	pensando	bool
 	comiendo	bool
+	esHombre 	bool
 }
 
 /*mujer*/
 
 type mujer struct {
-	edad		int
-	altura		float32
-	peso		float32
-	respirando	bool
-	pensando	bool
-	comiendo	bool
+	hombre
 }
 
 func (h *hombre) respirar() { h.respirando = true } 
 func (h *hombre) comer() { h.comiendo = true }
 func (h *hombre) pensar() { h.pensando = true }
-func (h *hombre) sexo() string { return "hombre" }
+func (h *hombre) sexo() string { 
+	if h.esHombre{
+		return "Hombre"
+	}else{
+	return "Mujer"
+	}
+}
 
-func (m *mujer) respirar() { m.respirando = true } 
-func (m *mujer) comer() {  m.comiendo = true }
-func (m *mujer) pensar() { m.pensando = true }
-func (m *mujer) sexo() string { return "mujer" }
 
-
-func humanosrespirando(hu humano)  {
+func humanosrespirando(hu humano){
 	hu.respirar()
 	fmt.Printf("Soy un/a %s y estoy respirando\n", hu.sexo())
 }
 
+/* Gnero anumal */
+
+
+/*perro*/
+
+type perro struct {
+	respirando	bool
+	comiendo	bool
+	carnivoro	bool
+}
+
+func (p *perro) respirar() { p.respirando = true } 
+func (p *perro) comer() { p.comiendo = true }
+func (p *perro) Escarnivoro() bool { return p.carnivoro }
+
+func animalesrespirar(an animal) {
+	an.respirar()
+	fmt.Println("Soy un animal y estoy respirando")
+}
+
+func animalescarnivoro (an animal) {
+	if an.Escarnivoro() == true {
+		return 1
+	}
+	return 0
+}
+
 func main()  {
+	tcarrnivoro := 0
+
 	Pedro := new(hombre)
+	Pedro.esHombre = true
 	humanosrespirando(Pedro)
 
 	Maria := new(mujer)
 	humanosrespirando(Maria)
+
+	Drogo := new(perro)
+	Drogo.carnivoro = true
+	animalesrespirar(Drogo)
+	tcarrnivoro =+ animalescarnivoro(Drogo)
+
+	fmt.Printf("Carnivoros %d", tcarrnivoro)
+
 }
